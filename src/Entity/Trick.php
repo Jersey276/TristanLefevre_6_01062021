244eb2ff85b488b1096ef9fbb04d7712510f8000
Tristan <tristan-lefevre@hotmail.fr>
2021-06-14 19:14:20 +0200

    add comment entity + add Comment entity + link Comment to Trick with 'ManytoOne' relation + link Comment to User with 'ManytoOne' relation + add new migration for include change on Trick + new Comment table + add createdAt and modifiedAt dateTime on Trick

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TrickRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -32,6 +34,27 @@ class Trick
      * @ORM\JoinColumn(nullable=false)
      */
     private TrickGroup $trickGroup;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="Tricks", orphanRemoval=true)
+     * @ORM\OrderBy({"id" = "DESC"})
+     */
+    private $comments;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $ModifiedAt;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -88,5 +111,59 @@ class Trick
     public function getRemoveUrl() : String
     {
         return "/tricks/". $this->getId() . "/remove";
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setTricks($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getTricks() === $this) {
+                $comment->setTricks(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTimeInterface
+    {
+        return $this->ModifiedAt;
+    }
+
+    public function setModifiedAt(?\DateTimeInterface $ModifiedAt): self
+    {
+        $this->ModifiedAt = $ModifiedAt;
+
+        return $this;
     }
 }
