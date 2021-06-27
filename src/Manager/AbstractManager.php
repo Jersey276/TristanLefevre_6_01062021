b0912a53f2ff498cd0237aaad21ab9c4e31fedb1
Tristan <tristan-lefevre@hotmail.fr>
2021-06-27 19:21:26 +0200

    add event for send mail +install eventDispatcher for add possibility of lunch event + add Email event with var and eventname consts = rename service file for respect PSR 4 violation = replace mail service by email event call = modify email template for

--- src/Manager/AbstractManager.php
+++ src/Manager/AbstractManager.php
@@ -4,10 +4,13 @@ namespace App\Manager;
 
 use App\Service\FileService;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 abstract class AbstractManager
 {
     protected EntityManagerInterface $doctrine;
 
     protected FileService $fileService;
+
+    protected EventDispatcherInterface $eventDispatcher;
 }
--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -4,27 +4,31 @@ namespace App\Manager;
 
 use App\Manager\TokenManager;
 use App\Entity\User;
+use App\Event\EmailEvent;
+use App\Service\FileService;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Mailer\MailerInterface;
+use Doctrine\ORM\ORMException;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserManager extends AbstractManager
 {
-
-    private MailerInterface $mailer;
     private UserPasswordHasherInterface $passwordEncoder;
     private TokenManager $tm;
 
-    public function __construct(EntityManagerInterface $doctrine,
-        MailerInterface $mailer,
+    public function __construct(
+        EntityManagerInterface $doctrine,
         UserPasswordHasherInterface $passwordEncoder,
-        TokenManager $tokenManager
-    )
-    {
+        TokenManager $tokenManager,
+        FileService $fileService,
+        EventDispatcherInterface $eventDispatcher
+    ) {
         $this->doctrine = $doctrine;
-        $this->mailer = $mailer;
         $this->passwordEncoder = $passwordEncoder;
         $this->tm = $tokenManager;
+        $this->fileService = $fileService;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function register(User $user) : void
@@ -39,13 +43,13 @@ class UserManager extends AbstractManager
         $this->doctrine->flush();
 
         // Send mail with new token for valid email
-        $this->tm->sendToken($user->getUsername(), $this->mailer, "email");
+        $this->tm->sendToken($user->getUsername(), "email");
     }
 
     public function forgotPassword(User $user) : void
     {
         // Send mail with new token for change password
-        $this->tm->sendToken($user->getEmail(), $this->mailer, "password");
+        $this->tm->sendToken($user->getEmail(), "password");
     }
 
     public function changePassword(User $user, String $token) : void
