002df9f578b87fd5df73c12b3207a7034a376343
Tristan <tristan-lefevre@hotmail.fr>
2021-06-28 02:40:11 +0200

    Add profile Controller / forms type + Add profile controller for manage all forms of profile index page + add profile forms, for modify avatar, password and email + add function on UserManager for modify user + add new translation on message

---
+++ src/Form/ProfilePasswordFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilePasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options) : void
+    {
+        $builder
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Password',
+                    'label_attr' => [
+                        'class' => 'input_group_text mb-0'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirm Password',
+                    'label_attr' => [
+                        'class' => 'input_group_text mb-0'
+                    ]
+                ]
+            ])
+            ->add('update', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-outline-success'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver) : void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -60,8 +60,64 @@ class UserManager extends AbstractManager
         $this->tm->useToken($token, 'changePassword', $user->getPassword());
     }
 
+    public function changePasswordProfile(User $user) : bool
+    {
+        try {
+            $user->setPassword($this->passwordEncoder->hashPassword($user, $user->getPassword()));
+            $this->doctrine->flush();
+        } catch (ORMException $e) {
+            return false;
+        }
+        return true;
+    }
+
+    public function changeEmailProfile(User $user) : bool
+    {
+        try {
+            $oldUser = $this->doctrine->getRepository(User::class)->find($user->getId());
+            if ($oldUser->getEmail() == $user->getEmail()) {
+                $user->setIsEmailCheck(false);
+            }
+            $this->doctrine->flush();
+            $token = $this->tm->generateToken($user, 'email');
+            $event = new EmailEvent($user->getEmail(), $token->getToken());
+            $this->eventDispatcher->dispatch($event, $event::NAME_UPDATE);
+        } catch (ORMException $e) {
+            return false;
+        }
+        return true;
+    }
+
+    public function changeAvatarProfile(User $user, UploadedFile $file) : bool
+    {
+        $user->setAvatar(
+            $this->fileService->upload(
+                $file,
+                '/images/avatar/',
+                'avatar'. $user->getUsername()
+            )
+        );
+        try {
+            $this->doctrine->flush();
+        } catch (ORMException $e) {
+            return false;
+        }
+        return true;
+    }
+
     public function validEmail(String $token) : bool
     {
         return $this->tm->useToken($token, "email");
+    }
+
+    public function removeUser(User $user) : bool
+    {
+        try {
+            $this->doctrine->remove($user);
+            $this->doctrine->flush();
+            return true;
+        } catch (ORMException $e) {
+            return false;
+        }
     }
 }
